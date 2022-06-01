051bb48a7ea023a47a3bd2b95837681f80baee01
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2022-06-01 16:08:27 +0200

    Test reverse array to have the last post first

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,12 +1,12 @@
 const auth = require('../middleware/auth')
 const express = require('express');
 const router = express.Router();
-
+const molter = require('../middleware/molter-config')
 const postsCtrl = require('../controller/postsC');
 
-router.get('/',auth , postsCtrl.getAllPosts)
+router.get('/',/*auth ,*/ postsCtrl.getAllPosts)
 router.get('/:id',auth , postsCtrl.getOnePosts)
-router.post('/',auth , postsCtrl.createOnePosts)
+router.post('/',auth , molter, postsCtrl.createOnePosts)
 router.put('/:id',auth , postsCtrl.updateOnePosts)
 router.delete('/:id',auth , postsCtrl.deleteOnePosts)
 
