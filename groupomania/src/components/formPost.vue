6692d295873de683f3336a865d4358723f65d01a
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2022-06-17 10:01:02 +0200

    Profil on Network create push profil done and Logout done + style

--- groupomania/src/components/formPost.vue
+++ groupomania/src/components/formPost.vue
@@ -1,40 +1,49 @@
 <template>
-  <form class="postForm" @submit.prevent="postForm" method="post">
-    <div class="postForm_input">
-      <label for="title">Post title:</label>
-      <input
-        type="text"
-        v-model="postData.title"
-        name="title"
-        id="title"
-        maxlength="150"
-        required
-        placeholder="Enter your title here..."
-      />
+  <div class="topPage">
+    <div class="profile">
+      <h4>Profil</h4>
+      <div class="profile_btn">
+        <button class="edit option" @click="goProfile"><i class="fa-solid fa-pencil fa-lg"></i></button>
+        <button class="logout option" @click="logout"><i class="fa-solid fa-right-from-bracket"></i></button>
+      </div>
     </div>
-    <div class="postForm_input">
-      <label for="content">Content: </label>
-      <textarea
-        v-model="postData.content"
-        name="content"
-        id="content"
-        maxlength="400"
-        required
-        placeholder="Enter your post here..."
-      ></textarea>
-    </div>
-    <div class="postForm_input">
-      <input
-        type="file"
-        name="image"
-        id="image"
-        accept="image/png, image/jpeg, image/jpg"
-      />
-    </div>
-    <div class="postForm_input">
-      <button class="postForm_btn">send</button>
-    </div>
-  </form>
+    <form class="postForm" @submit.prevent="postForm" method="post">
+      <div class="postForm_input">
+        <label for="title">Post title:</label>
+        <input
+          type="text"
+          v-model="postData.title"
+          name="title"
+          id="title"
+          maxlength="150"
+          required
+          placeholder="Enter your title here..."
+        />
+      </div>
+      <div class="postForm_input">
+        <label for="content">Content: </label>
+        <textarea
+          v-model="postData.content"
+          name="content"
+          id="content"
+          maxlength="400"
+          required
+          placeholder="Enter your post here..."
+        ></textarea>
+      </div>
+      <div class="postForm_input">
+        <input
+          type="file"
+          name="image"
+          id="image"
+          accept="image/png, image/jpeg, image/jpg"
+        />
+      </div>
+      <div class="postForm_input">
+        <button class="postForm_btn">send</button>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 export default {
@@ -73,51 +82,99 @@ export default {
             }
           })
       }
+    },
+    goProfile () {
+      this.$router.push('/profile')
+    },
+    logout () {
+      localStorage.clear()
+      this.$router.push('/')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .postForm {
+.topPage{
   display: flex;
-  flex-flow: column wrap;
-  width: 33%;
-  border: solid rgb(240, 10, 10) 2px;
-  padding: 1%;
-  margin: 2% 33% 4% 33%;
-  background-color: rgb(255, 215, 215);
-  box-shadow: rgba(78, 81, 102, 0.4) 0px 0px 0px 2px, rgba(78, 81, 102, 0.65) 0px 4px 6px -1px, rgba(78, 81, 102, 0.08) 0px 1px 0px;
-  &_input{
-    width:100%;
-    display: flex;
-    flex-flow:column nowrap;
-    label{
-      font-weight: bold;
-    }
-    input, #content{
-      margin: 2% 0%;
-    }
-  }
-  button{
-    display: flex;
-    justify-content: center;
-    width: 33%;
-    margin: 2% 33%;
-    padding: 1%;
-    border: solid #4E5166 2px;
-    border-radius: 5px;
-    font-weight: bolder;
-    box-shadow: rgba(78, 81, 102, 0.4) -4px 4px 4px 4px;
-  }
-  button:hover{
-    box-shadow: rgba(78, 81, 102, 0.6) -2px 2px 2px 2px;
+  flex-flow: row nowrap;
+  align-content: center;
+  .profile{
+    width: 5%;
+    margin: auto 2%;
+    padding: 0% 1% 1% 1%;
+    background-color: rgb(255, 215, 215);
     border: solid rgb(240, 10, 10) 2px;
+
+    &_btn{
+      display: flex;
+      justify-content: space-around;
+      .option{
+        border: solid 2px rgb(78, 81, 102);
+        border-radius: 20px;
+      }
+      .option:hover{
+        border: solid 2px red;
+      }
+      .edit{
+        padding: 7%;
+      }
+      .edit:hover{
+        color:orangered;
+      }
+      .logout{
+        padding: 7% 8%;
+      }
+      .logout:hover{
+        color: red;
+      }
+      .edit:active, .logout:active{
+        background-color: rgb(78, 81, 102);
+        color: white;
+      }
+    }
   }
-  button:active{
-    box-shadow: rgba(78, 81, 102, 0.8) 0px 2px 2px;
-    background: lighten(#4E5166, 10%);
-    color: white;
+  .postForm {
+    display: flex;
+    flex-flow: column wrap;
+    width: 33%;
+    border: solid rgb(240, 10, 10) 2px;
+    border-radius: 10px;
+    padding: 1%;
+    margin: 2% 33% 4% 21%;
+    background-color: rgb(255, 215, 215);
+    box-shadow: rgba(78, 81, 102, 0.4) 0px 0px 0px 2px, rgba(78, 81, 102, 0.65) 0px 4px 6px -1px, rgba(78, 81, 102, 0.08) 0px 1px 0px;
+    &_input{
+      width:100%;
+      display: flex;
+      flex-flow:column nowrap;
+      label{
+        font-weight: bold;
+      }
+      input, #content{
+        margin: 2% 0%;
+      }
+    }
+    button{
+      display: flex;
+      justify-content: center;
+      width: 33%;
+      margin: 2% 33%;
+      padding: 1%;
+      border: solid #4E5166 2px;
+      border-radius: 5px;
+      font-weight: bolder;
+      box-shadow: rgba(78, 81, 102, 0.4) -4px 4px 4px 4px;
+    }
+    button:hover{
+      box-shadow: rgba(78, 81, 102, 0.6) -2px 2px 2px 2px;
+      border: solid rgb(240, 10, 10) 2px;
+    }
+    button:active{
+      box-shadow: rgba(78, 81, 102, 0.8) 0px 2px 2px;
+      background: lighten(#4E5166, 10%);
+      color: white;
+    }
   }
 }
 </style>
--- groupomania/src/components/posts.vue
+++ groupomania/src/components/posts.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="allPost">
-    <h2 v-if="posts.length < 1">No contents for the moment</h2>
+    <h2 class="emptyApi" v-if="posts.length < 1">No contents for the moment</h2>
     <article
       v-else
       class="post"
@@ -8,43 +8,41 @@
       :key="post.id"
     >
       <div class="post_header">
-        <h4>{{ post.user.userName }}</h4>
+        <h3>{{ post.user.userName }}</h3>
         <div class="post_btn">
           <!-- v-if="user.id == post.userId || user.admin" -->
-          <button class="edit"><i class="fa-solid fa-pencil"></i></button>
-          <button class="delete">
+          <button class="edit option"><i class="fa-solid fa-pencil"></i></button>
+          <button class="delete option">
             <i class="fa-solid fa-xmark fa-lg"></i>
           </button>
         </div>
       </div>
-
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.content }}</p>
-
-      <div class="commentForm">
-        <div>
-          <textarea
-            v-model="comment_content"
-            name="commentForm_input"
-            id="comment_input"
-            minlength="2"
-            maxlength="150"
-            required
-            placeholder="Comment here..."
-          ></textarea>
-          <button class="commentForm_btn">
-            <i class="fa-solid fa-comment fa-lg"></i>
-          </button>
-        </div>
+      <div class="post_content">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.content }}</p>
+      </div>
+      <div class="post_commentForm">
+        <textarea
+          v-model="comment_content"
+          name="commentForm_input"
+          id="comment_input"
+          minlength="2"
+          maxlength="150"
+          required
+          placeholder="Comment here..."
+        ></textarea>
+        <button class="commentForm_btn">
+          <i class="fa-solid fa-comment fa-lg"></i>
+        </button>
       </div>
 
-      <div class="comment" v-for="comment of post.comments" :key="comment.id">
+      <div class="post_comment" v-for="comment of post.comments" :key="comment.id">
         <h4>{{ comment.user.userName }}</h4>
         <p>{{ comment.content }}</p>
         <div class="comment_btn">
           <!-- v-if="user.id == comment.userId || user.admin" -->
-          <button class="edit"><i class="fa-solid fa-pencil"></i></button>
-          <button class="delete">
+          <button class="edit option"><i class="fa-solid fa-pencil"></i></button>
+          <button class="delete option">
             <i class="fa-solid fa-xmark fa-lg"></i>
           </button>
         </div>
@@ -77,11 +75,115 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.emptyApi{
+  min-height: 100vh;
+}
 .post{
   border: solid 2px rgba(240, 10, 10, 0.8);
-  box-shadow: rgba(240, 10, 10, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px;
+  border-radius: 20px;
+  background-color: rgb(255, 215, 215);
+  box-shadow: rgba(240, 10, 10, 0.6) 3px 1px 8px 0px, rgba(255, 255, 255, 0.8) -3px -1px 8px 0px;
   width: 50%;
   margin: 0% 25% 2% 25%;
-
+  padding: 1%;
+  &_header{
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+    h3{
+      box-shadow: rgba(240, 10, 10, 0.5) 0px 0px 0px 3px;
+      background-color: white;
+      padding: 1%;
+      border-radius: 20px
+    }
+    .post_btn{
+      width: 10%;
+      display: flex;
+      justify-content: space-around;
+    }
+  }
+  &_content{
+    background-color: white;
+    padding: 1% 0%;
+    margin: 2% 0%;
+    border-radius: 20px;
+    box-shadow: rgb(240, 10, 10) 0px 20px 30px -10px;
+    h2{
+      text-decoration: underline;
+    }
+    p{
+      text-align: start;
+      padding-left: 2%;
+    }
+  }
+  &_commentForm{
+    padding: 2% 0%;
+    display: flex;
+    justify-content: center;
+    #comment_input{
+      width: 50%;
+    }
+    .commentForm_btn{
+      width: 6%;
+      margin: 0% 2%;
+      padding: 2% 0%;
+      border-radius: 30px;
+      border: solid 2px rgb(78, 81, 102);
+    }
+    .commentForm_btn:hover{
+      border: solid red 2px;
+      color: rgb(78, 81, 102);
+    }
+    .commentForm_btn:active{
+      color: white;
+      background-color: rgb(78, 81, 102);
+    }
+  }
+  &_comment{
+    margin: 2%;
+    padding: 0% 1%;
+    background-color: white;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    border-radius: 20px;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 3px 0px;
+    h4{
+      box-shadow: rgba(240, 10, 10, 0.5) 0px 0px 0px 3px;
+      padding: 1%;
+      margin: 1% 0.5%;
+      border-radius: 20px
+    }
+    .comment_btn{
+      width: 10%;
+      display: flex;
+      justify-content: space-around;
+    }
+  }
+  .option{
+    border: solid 2px rgb(78, 81, 102);
+    border-radius: 20px;
+  }
+  .option:hover{
+    border: solid 2px red;
+  }
+  .edit{
+    padding: 7% 8%;
+  }
+  .edit:hover{
+    color:orangered;
+  }
+  .delete{
+    padding: 6% 9%;
+  }
+  .delete:hover{
+    color: red;
+  }
+  .edit:active, .delete:active{
+    background-color: rgb(78, 81, 102);
+    color: white;
+  }
 }
 </style>
